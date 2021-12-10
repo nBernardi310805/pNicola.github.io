4a5aafcbde1d15c4e24b1de37430447a243dd538
nBernardi05 <84014354+nBernardi05@users.noreply.github.com>
2021-12-10 15:56:02 +0100

    Add files via upload

---
+++ doc/cCf.cpp
@@ -0,0 +1,107 @@
+//Programma calcolatrice
+#include <iostream>
+using namespace std;
+
+void opzioneSomma();
+int somma(int, int);
+void opzioneSottrazione();
+int sottrazione(int, int);
+void opzioneMoltiplicazione();
+int moltiplicazione(int, int);
+void opzioneDivisione();
+int divisione(int, int);
+
+
+int main(){
+    int opzione;
+    bool continua=true;
+    while(continua){
+        cout<<"1 - Addizione"<<endl;
+        cout<<"2 - Sottrazione"<<endl;
+        cout<<"3 - Moltiplicazione"<<endl;
+        cout<<"4 - Divisione"<<endl;
+        cout<<"99 - Fine"<<endl<<endl;
+        cout<<"Scegliere una delle opzioni sopraelencate: ";
+        cin>>opzione;
+        switch (opzione) {
+            case 1:
+                opzioneSomma();
+                break;
+            case 2:
+                opzioneSottrazione();
+                break;
+            case 3:
+                opzioneMoltiplicazione();
+                break;
+            case 4:
+                opzioneDivisione();
+                break;
+            case 99:
+                cout<<"Grazie per aver usato il mio programma :)"<<endl;
+                continua=false;
+                break;
+        }
+    }
+    return 0;
+}
+
+void opzioneSomma(){
+    int a, b;
+    cout<<"ADDIZIONE:"<<endl;
+    cout<<"Dammi il primo addendo --> ";
+    cin>>a;
+    cout<<"Dammi il secondo addendo --> ";
+    cin>>b;
+    cout<<"La somma degli addendi "<<a<<" e "<<b<<" e' "<<somma(a, b)<<endl<<endl<<endl;
+}
+
+int somma(int a, int b){
+    int totale;
+    totale=a+b;
+    return totale;
+}
+
+void opzioneSottrazione(){
+    int a, b;
+    cout<<"SOTTRAZIONE:"<<endl;
+    cout<<"Dammi il minuendo --> ";
+    cin>>a;
+    cout<<"Dammi il sottraendo --> ";
+    cin>>b;
+    cout<<"La differenza tra "<<a<<" e "<<b<<" e' "<<sottrazione(a, b)<<endl<<endl<<endl;
+}
+int sottrazione(int a, int b){
+    int differenza;
+    differenza=a-b;
+    return differenza;
+}
+
+void opzioneMoltiplicazione(){
+    int a, b;
+    cout<<"MOLTIPLICAZIONE:"<<endl;
+    cout<<"Dammi il moltiplicando --> ";
+    cin>>a;
+    cout<<"Dammi il moltiplicatore --> ";
+    cin>>b;
+    cout<<"Il prodotto di "<<a<<" e "<<b<<" e' "<<moltiplicazione(a, b)<<endl<<endl<<endl;
+}
+int moltiplicazione(int a, int b){
+    int prodotto;
+    prodotto=a*b;
+    return prodotto;
+}
+
+void opzioneDivisione(){
+    int a, b;
+    cout<<"DIVISIONE:"<<endl;
+    cout<<"Dammi il dividendo --> ";
+    cin>>a;
+    cout<<"Dammi il divisore --> ";
+    cin>>b;
+    cout<<"Il quoto di "<<a<<" e "<<b<<" e' "<<divisione(a, b)<<endl<<endl<<endl;
+}
+int divisione(int a, int b){
+    int quoto;
+    quoto=a/b;
+    return quoto;
+}
