c8f518f8a48dafc623702055d3836c4bcc249efd
nBernardi05 <84014354+nBernardi05@users.noreply.github.com>
2022-01-15 18:15:09 +0100

    Update fatt.cpp

--- doc/fatt.cpp
+++ doc/fatt.cpp
@@ -1,5 +1,6 @@
 //fattoriale
 #include <iostream>
+//creato da Nicola Bernardi, caricato su https://portalenicola.it/cpp
 using namespace std;
 int fattoriale(int);
 int main(){
@@ -7,12 +8,12 @@ int main(){
     cout<<"Dammi il numero di cui vuoi calcolare il fattoriale: ";
     cin>>n;
     cout<<fattoriale(n)<<endl;
-}
+}//creato da Nicola Bernardi, caricato su https://portalenicola.it/cpp
 int fattoriale(int n){
     int fatt=1;
     while(n>1){
         fatt=fatt*n;
         n--;
-    }
+    }//creato da Nicola Bernardi, caricato su https://portalenicola.it/cpp
     return fatt;
 }
