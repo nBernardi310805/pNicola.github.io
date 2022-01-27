29c2a21aa7394e3e412ae545cc89d691153f883c
nBernardi310805 <91664287+nBernardi310805@users.noreply.github.com>
2022-01-27 17:19:34 +0100

    Add files via upload

---
+++ js/altro.js
@@ -0,0 +1,48 @@
+function down1(){
+    document.getElementById('dw1').innerText="Download --> "
+}
+function downE1() {
+    document.getElementById('dw1').innerText="Download"
+}
+function down2(){
+    document.getElementById('dw2').innerText="Download --> "
+}
+function downE2() {
+    document.getElementById('dw2').innerText="Download"
+}
+function down3(){
+    document.getElementById('dw3').innerText="Download --> "
+}
+function downE3() {
+    document.getElementById('dw3').innerText="Download"
+}
+function down4(){
+    document.getElementById('dw4').innerText="Download --> "
+}
+function downE4() {
+    document.getElementById('dw4').innerText="Download"
+}
+function down5(){
+    document.getElementById('dw5').innerText="Download --> "
+}
+function downE5() {
+    document.getElementById('dw5').innerText="Download"
+}
+function down6(){
+    document.getElementById('dw6').innerText="Download --> "
+}
+function downE6() {
+    document.getElementById('dw6').innerText="Download"
+}
+function down7(){
+    document.getElementById('dw7').innerText="Download --> "
+}
+function downE7() {
+    document.getElementById('dw7').innerText="Download"
+}
+function down8(){
+    document.getElementById('dw8').innerText="Download --> "
+}
+function downE8() {
+    document.getElementById('dw8').innerText="Download"
+}
