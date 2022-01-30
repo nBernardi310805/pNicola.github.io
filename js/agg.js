e5919d5fefe3674eb04e8c2b63e4d872f2ac2919
nBernardi310805 <91664287+nBernardi310805@users.noreply.github.com>
2022-01-30 16:36:27 +0100

    Update agg.js

--- js/agg.js
+++ js/agg.js
@@ -3,12 +3,14 @@ function premium(){
     switch(pId) {
         case 'nBern.psl05':
             alert("Benvenuto Padrone")
-            document.getElementById('msgL0').innerText="Accesso effettuato come Nicola Bernardi"
-
+            document.getElementById('msgL').innerText="Accesso effettuato come Nicola Bernardi"
+            document.getElementById('msgc').innerText="Verrà creata una pagina per gli appunti; \n Verranno caricati alcuni progetti in HTML, CSS, JS"
+            
             break
         case 'bAnCh.amolagrammatica':
             alert("Benvenuta prof Annachiara Barizza")
-            document.getElementById('msgL0').innerText="Accesso effettuato come Annachiara Barizza"
+            document.getElementById('msgL').innerText="Accesso effettuato come Annachiara Barizza"
+            document.getElementById('msgc').innerText="Verrà creata una pagina per gli appunti; \n Verranno caricati alcuni progetti in HTML, CSS, JS"
             
             break
         
