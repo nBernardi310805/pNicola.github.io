8aa527ebff1688aa186a761da1e58544036136c2
nBernardi05 <84014354+nBernardi05@users.noreply.github.com>
2022-02-11 09:39:18 +0100

    Update diffValArr.cpp

--- doc/diffValArr.cpp
+++ doc/diffValArr.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #define N 7
+//programma per orfinare l'array
 using namespace std;
 int diffArray(int []);
 int main()
-{
-    int arr[N];
+{//by Nicola Bernardi
+    int arr[N/*portalenicola.it/didattica/cpp*/];
     for(int i=0; i<N; i++){
         cout<<"Valorizza la posizione "<<i<<" dell'array: --> ";
         cin>>arr[i];
-    }
+    }//portalenicola.it/didattica/cpp
     cout<<"La differenza tra il valore massimo e quello minimo inserito e' "<<diffArray(arr)<<endl;
     return 0;
 }
-
+//portalenicola.it
 int diffArray(int arr[]){
     int mx=arr[0], mn=arr[0];
     for(int i=1; i<N; i++){
         if(arr[i]>mx){
             mx=arr[i];
-        }
+        }//portalenicola.it/didattica/cpp
         if(arr[i]<mn){
             mn=arr[i];
         }
