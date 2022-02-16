0ae3c801127ba0e59d2c66f8dc95c68b0f1f502d
nBernardi310805 <91664287+nBernardi310805@users.noreply.github.com>
2022-02-16 17:27:28 +0100

    Update inter.js

--- js/inter.js
+++ js/inter.js
@@ -1,4 +1,4 @@
-
+//anche questa pagina ve la lascio visibile. Di nulla :)
 function inizia(){
     document.getElementById('h').contentEditable="true"
     document.getElementById('m').contentEditable="true"
@@ -8,8 +8,6 @@ function inizia(){
 function convS(){
     let gg, hh, mm, ss, risultato = 0
     
-
-
     if(document.getElementById('g').value!=''){
         risultato = risultato + document.getElementById('g').value*86400
     }
@@ -27,4 +25,23 @@ function convS(){
     
     document.getElementById('ris').innerHTML=" " + risultato 
 
+}
+function random(){
+    let a = document.getElementById('min').valueAsNumber
+    let b = document.getElementById('max').valueAsNumber
+    let c=0
+    if(isNaN(b)){
+        b=999000000000
+    }
+    
+    c = (b-a)
+    let ris = Math.floor(Math.random() * c) + a
+    if(isNaN(ris)){
+        alert('Non è che hai dimenticato di inserire qualcosa?')
+    }
+    if(isNaN(a)){
+        document.getElementById('risult').innerText="Sì, ma almeno il valore di partenza mettilo!"
+    }else{
+        document.getElementById('risult').innerText=+ris
+    }
 }
