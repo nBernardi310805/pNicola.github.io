6887b1830db9bf74816fd4c60dff9999dfd5be58
nBernardi05 <84014354+nBernardi05@users.noreply.github.com>
2022-05-22 13:41:54 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,46 @@
 function importante(){
-    alert('IMPORTANTE:\nA fine giugno mi scade il dominio portalenicola.it.\nNel caso non riuscissi a rinnovare il dominio, il nuovo dominio sarà "ilportaledinicola.it". Spero di riuscire a tenere il dominio attuale ("portalenicola.it")')
+    //alert('IMPORTANTE:\nA fine maggio mi scade il dominio portalenicola.it.\nNel caso non riuscissi a rinnovare il dominio, il nuovo dominio sarà "ilportaledinicola.it". Spero di riuscire a tenere il dominio attuale ("portalenicola.it")')
+}
+
+function usr(){
+    let cont = document.getElementById('nUtenti')
+    for(let i=1; i<=252; i++){
+        //cont.innerText = i
+        //setTimeout(aggiorna(i, cont), 1000)
+        setInterval(1000, cont.innerText = i)
+        console.log(i)
+    }
+}
+
+function aggiorna(n, dove){
+    dove.innerText = n
+    return
+}
+
+function countdown(){
+
+  const total = Date.parse('2022-06-08')-Date.parse(Date())
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+    /*console.log(total)
+    console.log(seconds)
+    console.log(minutes)
+    console.log(hours)
+    console.log(days) */
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function ggRim(){
+    setInterval(modifica, 1000)
+}
+function modifica(){
+    document.getElementById('cosafaccio').innerText = countdown().days + 'gg, ' + countdown().hours + 'h, ' + countdown().minutes + "', " + countdown().seconds + "'' alla fine della scuola"
 }
