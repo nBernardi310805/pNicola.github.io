356a8152ab34c614b4e62ba3c40115c72c5444b8
nBernardi05 <84014354+nBernardi05@users.noreply.github.com>
2022-06-17 12:14:56 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 function importante(){
-    //alert('IMPORTANTE:\nA fine maggio mi scade il dominio portalenicola.it.\nNel caso non riuscissi a rinnovare il dominio, il nuovo dominio sarà "ilportaledinicola.it". Spero di riuscire a tenere il dominio attuale ("portalenicola.it")')
+    alert('IMPORTANTE:\nIl 25 giugno mi scade il dominio portalenicola.it.\n Il nuovo dominio sarà "ilportaledinicola.it".')
 }
 
 function usr(){
