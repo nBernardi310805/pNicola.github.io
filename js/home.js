fe39b8fe60b22e89c8d8684c2a976dc9232ebbcf
nBernardi3108 <84276165+nBernardi3108@users.noreply.github.com>
2023-03-14 18:46:45 +0100

    patch 4.3.5.8

--- js/home.js
+++ js/home.js
@@ -1,34 +1,39 @@
+addEventListener('DOMContentLoaded', ggRim)
 function importante(){
     alert('IMPORTANTE:\nIl 25 giugno mi scade il dominio portalenicola.it.\n Il nuovo dominio sarà "ilportaledinicola.it".')
 }
-
+let i = 0
 function usr(){
-    let cont = document.getElementById('nUtenti')
-    for(let i=1; i<=252; i++){
-        //cont.innerText = i
-        //setTimeout(aggiorna(i, cont), 1000)
-        setInterval(1000, cont.innerText = i)
-        console.log(i)
-    }
+  //cont.innerText = i
+  //setTimeout(aggiorna(i, cont), 1000)
+
+  console.log(i)
+  if(i> 251)
+    return
+    setInterval(500, prova)
+    i++
+}
+function prova(){
+  let cont = document.getElementById('nUtenti')
+  cont.innerText = "ciao"
+  setInterval(500, usr)
 }
 
 function aggiorna(n, dove){
     dove.innerText = n
     return
 }
-
+/**
+ * Seriamente devo dirti cosa fa questa funzione? Non si capisce dal nome
+ * che poi al tempo questa funzione la presi da internet
+ * @returns 
+ */
 function countdown(){
-
-  const total = Date.parse('2022-06-08')-Date.parse(Date())
+  const total = Date.parse('2023-06-10')-Date.parse(Date())
   const seconds = Math.floor( (total/1000) % 60 );
   const minutes = Math.floor( (total/1000/60) % 60 );
   const hours = Math.floor( (total/(1000*60*60)) % 24 );
   const days = Math.floor( total/(1000*60*60*24) );
-    /*console.log(total)
-    console.log(seconds)
-    console.log(minutes)
-    console.log(hours)
-    console.log(days) */
   return {
     total,
     days,
@@ -37,10 +42,15 @@ function countdown(){
     seconds
   };
 }
-
+/**
+ * chiama la funzione modifica ogni secondo
+ */
 function ggRim(){
     setInterval(modifica, 1000)
 }
+/**
+ * crea la stringa da scrivere
+ */
 function modifica(){
     document.getElementById('cosafaccio').innerText = countdown().days + 'gg, ' + countdown().hours + 'h, ' + countdown().minutes + "', " + countdown().seconds + "'' alla fine della scuola"
 }
---
+++ js/pageUtil.js
@@ -0,0 +1,12 @@
+/**
+ * In BETA.
+ * lavorerò a questo file se troverò la voglia di farlo
+ */
+function creaNav(){
+    let title = document.querySelector('title').innerText
+    title = title.split(' -')
+}
+
+function getById(id){
+    return document.getElementById(id)
+}
