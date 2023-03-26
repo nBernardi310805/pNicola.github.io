f7327087be22014fb681fa46f15e699dc85569fc
nBernardi3108 <84276165+nBernardi3108@users.noreply.github.com>
2023-03-26 15:49:51 +0200

    time machine

--- js/timemachine.js
+++ js/timemachine.js
@@ -1,5 +1,6 @@
 addEventListener('DOMContentLoaded', ggRim)
 addEventListener('DOMContentLoaded', premessa)
+// mi si è buggato il commit. :(
 /**
  * Seriamente devo dirti cosa fa questa funzione? Non si capisce dal nome
  * che poi al tempo questa funzione la presi da internet
