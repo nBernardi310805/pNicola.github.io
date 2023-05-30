568933fe3101536ac8205e86d18e6ae69263dae2
nBernardi3108 <84276165+nBernardi3108@users.noreply.github.com>
2023-05-30 17:02:27 +0200

    The logo doesn't load issue fixed

--- js/pageUtil.js
+++ js/pageUtil.js
@@ -18,7 +18,7 @@ function creaNav(){     // ` `
     let navbar_name = ['Home', 'Sfondi', 'Didattica', 'Trucchi', 'Contatti', 'Interattivo']
     const THIS_PAGE = window.location.href
     let navbar_link = ['/', '/sfondi', '/didattica', '/trucchi', '/whoami', '/interattivo']
-    let html = '<a href="/tulipano.html"><img src="/images/LRepNew.png" id="ii" alt="logo"></a><h1 class="titol" id="titol">Il Portale di Nicola</h1>'
+    let html = '<a href="/tulipano.html"><img src="/images/lRepNew.png" id="ii" alt="logo"></a><h1 class="titol" id="titol">Il Portale di Nicola</h1>'
     html += '<nav><h2 class="nav1" id="modTut">'
     for(let i=0; i<navbar_name.length; i++){
         html += '<a '
